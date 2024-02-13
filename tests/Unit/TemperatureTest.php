341636a38de11abc7034ee30c03e9caa511202b1
N0FreeLunch <consilience90@gmail.com>
2024-02-13 23:33:41 +0900

    mockery 예제 코드 추가

---
+++ tests/Unit/TemperatureTest.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * php artisan test tests/Unit/TemperatureTest.php
+ */
+
+use \PHPUnit\Framework\TestCase;
+
+class Temperature
+{
+    private $service;
+
+    public function __construct($service)
+    {
+        $this->service = $service;
+    }
+
+    public function average()
+    {
+        $total = 0;
+        for ($i=0; $i<3; $i++) {
+            $total += $this->service->readTemp();
+        }
+        return $total/3;
+    }
+}
+
+class TemperatureTest extends TestCase
+{
+    public function tearDown(): void
+    {
+        Mockery::close();
+    }
+
+    public function testGetsAverageTemperatureFromThreeServiceReadings()
+    {
+        $service = Mockery::mock('service');
+        $service->shouldReceive('readTemp')
+            ->times(3)
+            ->andReturn(10, 12, 14);
+
+        $temperature = new Temperature($service);
+
+        $this->assertEquals(12, $temperature->average());
+    }
+}
